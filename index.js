bdfedf5432119f8f56dedc11aeac3b5559b91b45
mhinton123 <matthewhinton@hotmail.co.uk>
2024-01-23 15:19:05 +0000

    noun meanings completed

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/pike')
+const searchBtn = document.getElementById('search-btn')
+const searchBar = document.getElementById('searchbar')
+const mainContainer = document.getElementById('main-container')
+const searchBarWrapper = document.getElementById('searchbar-wr')
+const audioPlayBtn = document.getElementById('audio-play-btn')
+const audioPlayer = document.getElementById('audio-player')
+
+// renders page when the user clicks search btn
+searchBtn.addEventListener('click', function(e){
+    
+    // check iput is not empty
+    if(searchBar.value) {
+        
+        // reset searchbar border
+        searchBarWrapper.style.border = 'none'
+
+        renderWordHtml()
+    }
+    else {
+
+        // err msg
+        mainContainer.innerHTML = `<p class="err-msg">Whoops, can't be empty...</p>`
+        searchBarWrapper.style.border = 'solid 1px red'
+    }
+})
+
+
+// plays audio when btn is clicked
+audioPlayBtn.addEventListener('click', function(e){
+
+    audioPlayer.play()
+
+})
+
+
+async function renderWordHtml(){
+
+    mainHtml = ''
+    const searchInput = searchBar.value
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput}`
+
+    // api call
+    fetch(url)
   .then(response => response.json()) // assuming the response is JSON
   .then(data => {
 
+    
+    renderWordTitle(data[0].word)
+    renderAudioQue(data[0].phonetic)
+    renderAudioSource(data)
+    renderNounMeanings(data)
+    
     // all Data
     console.log(data);
 
     // word-title
     console.log(data[0].word);
     
-    // audio-que
+    // audio-que (returns undefined if not found )
     try {
         console.log(data[0].phonetic);
     }
@@ -30,7 +78,8 @@ fetch('https://api.dictionaryapi.dev/api/v2/entries/en/pike')
 
     // noun-meanings-list (loop over arr and target .definition) (cap to 3)
     try {
-        console.log(data[0].meanings[0].definitions.slice(0, 3))
+        console.log(data[0].meanings[0].definitions.slice(0, 3).map(noun => noun.definition))
+
     }
     catch {
         console.log('Nouns Not Found')
@@ -38,7 +87,7 @@ fetch('https://api.dictionaryapi.dev/api/v2/entries/en/pike')
     
     // synonyms-list (loop over arr) (cap to 3)
     try {
-        console.log(data[0].meanings[0].synonymsslice(0, 3))
+        console.log(data[0].meanings[0].synonyms.slice(0, 3))
     }
     catch {
         console.log('Synonyms Not Found')
@@ -68,3 +117,63 @@ fetch('https://api.dictionaryapi.dev/api/v2/entries/en/pike')
   .catch(error => {
     console.error('Error fetching data:', error);
   });
+
+}
+
+
+function renderWordTitle(word) {
+
+    if (word) {
+        const wordEl = document.getElementById('word-title')
+        wordEl.innerText = word
+    }
+}
+
+function renderAudioQue(audioQue) {
+
+    const audioQueEl = document.getElementById('audio-que')
+    
+    if (audioQue) {
+        audioQueEl.innerText = audioQue
+    }
+    else {
+        audioQueEl.innerHTML = ''
+    }
+}
+
+function renderAudioSource(data) {
+
+    const audioContainer = document.getElementById('audio-play-btn-wr')
+
+    try {
+        const audioArr = data[0].phonetics
+        const audioFileLink = audioArr.find(audioObj => audioObj.audio !== '')
+
+        const sourceEl = document.getElementById('audio-source')
+        sourceEl.src = audioFileLink.audio
+    }
+    catch {
+        audioContainer.innerHTML = ''
+    }
+}
+
+function renderNounMeanings(data) {
+
+    const meaningsList = document.getElementById('noun-meanings-list')
+    console.log(data[0].meanings[0].definitions.slice(0, 3).map(noun => noun.definition))
+
+    try {
+        
+        const meaningsArr = data[0].meanings[0].definitions.slice(0, 3).map(noun => noun.definition)
+        let html = ''
+
+        meaningsArr.forEach(meaning => {
+            html += `<li>${meaning}</li>`
+        })
+
+        meaningsList.innerHTML = html
+    }
+    catch {
+        meaningsList.innerHTML = 'No meanings found'
+    }
+}
